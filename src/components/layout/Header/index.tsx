1bf7a88c5343dc3c347cde1c4f1ad8ffa4880fdc
sopoour <sophia.auer@googlemail.com>
2023-10-25 17:19:53 +0200

    remove hover effect on burger menu

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -63,10 +63,6 @@ const Line = styled.span<{ $isActive: boolean }>`
         transform: translateY(-4px) rotate(-45deg);
       }
     `}
-
-  &:hover {
-    background-color: ${theme.colors.accent.green} !important;
-  }
 `;
 
 const BurgerMenu = styled.button`
